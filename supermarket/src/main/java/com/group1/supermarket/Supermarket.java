9ba0910c27f9494777fa9f1b341d9b905c065c23
Giovanny Leal <giovanny.leala@gmail.com>
2023-07-18 21:01:46 -0500

    Create project to start working in the activity

---
+++ supermarket/src/main/java/com/group1/supermarket/Supermarket.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.group1.supermarket;
+
+/**
+ *
+ * @author giova
+ */
+public class Supermarket {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
